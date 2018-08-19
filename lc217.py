da184371d6e5f4eb2b32efe28e3bc62b19232dee
xiangru <paulqian@sjtu.edu.cn>
2018-08-19 13:09:02 -0400

    LC121 - Accepted at 12.42% - Aug 19, 2018

---
+++ lc217.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
+
+
+a = Solution()
+print(a.containsDuplicate([1,2,3,1]))
